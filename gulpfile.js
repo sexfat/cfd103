a06bc2c6e9721632b2a90204df08c0c20ac0298b
sexfat <bryant6831@gmail.com>
2021-12-01 14:40:05 +0800

    add series

--- gulpfile.js
+++ gulpfile.js
@@ -14,3 +14,17 @@ function taskcon(cb){
 // 任務的輸出 -> print cmd screen
 exports.console = taskcon;
 
+
+
+function taskA(cb){
+  console.log('任務A');
+  cb(); 
+}
+
+function taskB(cb){
+  console.log('任務B');
+  cb(); 
+}
+
+exports.a = series(taskA , taskB);  // 依序執行
+exports.b = parallel(taskA , taskB);// 同時執行
