2cbcd1dd8f3b9bd486bd78a222f44eb2e734f75c
sexfat <bryant6831@gmail.com>
2021-12-01 16:18:08 +0800

    add html template

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,9 @@ const {
     watch
 } = require('gulp');
 
-function taskcon(cb){
-  console.log('第一個任務');
-  cb(); 
+function taskcon(cb) {
+    console.log('第一個任務');
+    cb();
 }
 
 // 任務的輸出 -> print cmd screen
@@ -16,18 +16,18 @@ exports.console = taskcon;
 
 
 
-function taskA(cb){
-  console.log('任務A');
-  cb(); 
+function taskA(cb) {
+    console.log('任務A');
+    cb();
 }
 
-function taskB(cb){
-  console.log('任務B');
-  cb(); 
+function taskB(cb) {
+    console.log('任務B');
+    cb();
 }
 
-exports.a = series(taskA , taskB);  // 依序執行
-exports.b = parallel(taskA , taskB);// 同時執行
+exports.a = series(taskA, taskB);  // 依序執行
+exports.b = parallel(taskA, taskB);// 同時執行
 
 
 //sass 編譯
@@ -35,19 +35,33 @@ exports.b = parallel(taskA , taskB);// 同時執行
 const sass = require('gulp-sass')(require('sass'));
 
 // 任務函式
-function sassstyle(){
-  return src('./sass/style.scss') //來源檔案
-  .pipe(sass.sync().on('error', sass.logError))// sass編譯
-  .pipe(dest('css'))// 目的地檔案
+function sassstyle() {
+    return src('./sass/style.scss') //來源檔案
+        .pipe(sass.sync().on('error', sass.logError))// sass編譯
+        .pipe(dest('css'))// 目的地檔案
 }
 
-//任務輸出
+//sass任務輸出
 exports.sass = sassstyle;
+
+// html template
+const fileinclude = require('gulp-file-include');
+function includeHTML() {
+    return src('*.html')
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+        .pipe(dest('dist'));
+}
+exports.html = includeHTML;
+
+
 
 
 //監看所有任務
-function watchsass(){
-  watch(['sass/*.scss' ,'sass/**/*.scss'] , sassstyle); // 監看哪些檔案（檔案變動）並執行sassstyle 
+function watchsass() {
+    watch(['sass/*.scss', 'sass/**/*.scss'], sassstyle); // 監看哪些檔案（檔案變動）並執行sassstyle 
 }
 
 exports.w = watchsass;
