e6f2b7249878eaa6150302abdfbb0c041657d51e
sexfat <bryant6831@gmail.com>
2021-12-01 15:18:54 +0800

    add gulp sass

--- gulpfile.js
+++ gulpfile.js
@@ -28,3 +28,20 @@ function taskB(cb){
 
 exports.a = series(taskA , taskB);  // 依序執行
 exports.b = parallel(taskA , taskB);// 同時執行
+
+
+//sass 編譯
+
+const sass = require('gulp-sass')(require('sass'));
+
+// 任務函式
+function sassstyle(){
+  return src('./sass/style.scss') //來源檔案
+  .pipe(sass.sync().on('error', sass.logError))// sass編譯
+  .pipe(dest('css'))// 目的地檔案
+}
+
+//任務輸出
+exports.sass = sassstyle;
+
+
