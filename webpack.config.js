f2ebc3c7869db2010bf0cec36e779fab8a71b8b6
sexfat <bryant6831@gmail.com>
2021-12-16 11:50:35 +0800

    app

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
 
-    entry: './app.js', // 入口文件
+    entry: './src/app.js', // 入口文件
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
