8cc401bce25013312d98d758933d92493c2036a5
sexfat <bryant6831@gmail.com>
2021-12-16 12:02:58 +0800

    add multi

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,14 @@ const path = require('path');
 
 
 module.exports = {
-    entry: ['./src/app.js' ,'./src/app2.js'], // 入口文件
+    // entry: ['./src/app.js' ,'./src/app2.js'], // 入口文件 //array
+    entry: {
+      home : './src/app.js',
+      about : './src/app2.js' 
+    }, // 入口文件 //object
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },             // 出口文件
     //module: {},              // 處裡對應模組
     //plugins: [],             // 對應的插件
