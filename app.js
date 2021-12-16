da4c96bbec0b634d5788eee2e51397d1f648b1bf
sexfat <bryant6831@gmail.com>
2021-12-16 10:29:06 +0800

    add jquery

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
+import $ from 'jquery' ;
+
 console.log('webpack start');
 
 const x = (x , y) => x * y;
 document.getElementById('app').innerHTML =  x(200 , 800);
+
+$('body').css('background-color' , '#f20');
 
