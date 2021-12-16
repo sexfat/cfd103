5906305599a35bd13e86ad78755225831a05dfe4
sexfat <bryant6831@gmail.com>
2021-12-16 16:48:03 +0800

    add

--- src/app.js
+++ src/app.js
@@ -7,9 +7,9 @@ console.log('app1');
 const x = (x , y) => x * y + 'NT$';
 document.getElementById('app').innerHTML =  x(900 , 800);
 // jq
-$('body').css('background-color' , 'green');
+$('body').css('background-color' , '#eee');
 
 // gsap
-gsap.to('.box' , {x: 200 , y : 300});
+gsap.to('.box' , {x: 200 , y : 300 , rotation : 360 , repeat : -1 , yoyo: true});
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const webpack  = require('webpack');
 
 module.exports = {
     // entry: ['./src/app.js' ,'./src/app2.js'], // 入口文件 //array
@@ -13,24 +17,42 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].js'
     },             // 出口文件
-    module: {
+   module: {
         rules: [{
             // 格式
             test: /\.(sass|scss|css)$/,
-            //順序是由下到上 sass > css > style
+            //順序是由下到上 css > style
             use: [{
-                loader: MiniCssExtractPlugin.loader,
-                options: {
-                  publicPath: './dist'
-                }
-              },
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                      publicPath: './dist'
+                    }
+                  },
+                // 'style-loader',//跟MiniCssExtractPlugin 會衝突所以要關掉
                 'css-loader',
                 'sass-loader'
             ],
-        }]
+        },
+        //babel loader
+        {
+            test: /\.(js)$/,
+            exclude: /(node_modules)/,
 
-    },               // 處裡對應模組
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }],
+            include: path.resolve(__dirname, 'src'),
+        },
+
+      ]
+
+    },              // 處裡對應模組
     plugins: [
+         //清理舊的檔案
+        new CleanWebpackPlugin(),
         new MiniCssExtractPlugin({
             filename: "./[name].css"
         }),
@@ -49,8 +71,19 @@ module.exports = {
             //來源
             filename : 'about.html'
             // 目的地
-        })
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+          })
     ],             // 對應的插件
-    //devServer: {},           // 服務器配置
+    devServer: {
+        contentBase: './dist',
+        host: 'localhost',
+        port: 3200,
+        // 指定首頁檔案
+        index: 'index.html',
+        open: true
+    },          // 服務器配置
     mode: 'development'      // 開發模式配置 production development
 }
