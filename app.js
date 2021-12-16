60c8ffbca8fd864e146aaee3f85a266a53524444
sexfat <bryant6831@gmail.com>
2021-12-16 10:25:32 +0800

    add webpack

---
+++ app.js
@@ -0,0 +1,5 @@
+console.log('webpack start');
+
+const x = (x , y) => x * y;
+document.getElementById('app').innerHTML =  x(200 , 800);
+
--- src/js/main.js
+++
@@ -1,2 +0,0 @@
-const x = (x, y) => x * y;
-document.getElementById('app').innerHTML = x(90, 10);
